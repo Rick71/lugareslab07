24fcc2710a2f5e1f7e19a8be2899325b19f351c2
Ricardo Vera <ricardo@uag.com>
2015-02-17 00:53:07 -0600

    Initial Commit

---
+++ lugareslab07Tests/lugareslab07Tests.m
@@ -0,0 +1,40 @@
+//
+//  lugareslab07Tests.m
+//  lugareslab07Tests
+//
+//  Created by Ricardo Vera on 17/02/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface lugareslab07Tests : XCTestCase
+
+@end
+
+@implementation lugareslab07Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
